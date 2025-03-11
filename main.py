f3cd381f37c085c1d6a0758f2a4e96099138953e
EstefaniaRubiano <rubianoestefania15@gmail.com>
2025-03-11 10:08:23 +0100

    falta fer les proves i el readme

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 from typing import List
-from fastapi import FastAPI
-from services import read
+from fastapi import FastAPI, Depends
+from psycopg2.errorcodes import DATABASE_DROPPED
 
+from services import read
+from sqlmodel import SQLModel, create_engine, Session
+from dotenv import load_dotenv
+from services import user
+import os
+
+# Crear l'aplicació FastAPI
 app = FastAPI()
 
-@app.get("/root", response_model=List[dict])
-async def read_root():
-    result = read.registre()
+# 1. Carregar variables d'entorn
+load_dotenv()
+
+# 2. Configurar la connexió a PostgreSQL
+DATABASE_URL = os.getenv("DATABASE_URL") # Obtenir la URL de connexió
+engine = create_engine(DATABASE_URL) # Crear l'engine de connexió
+
+# 3. Crear les taules a la base de dades
+SQLModel.metadata.create_all(engine)
+
+# 4. Dependència per obtenir la sessió de la base de dades
+def get_db():
+    db = Session(engine)
+    try:
+        yield db
+    finally:
+        db.close()
+
+# 5. Endpoint per afegir usuaris
+@app.get("/users/", response_model=list[dict])
+def read_user(db:Session = Depends(get_db)):
+    result = user.get_all_users(db)
+    return result
+
+# 6. Endpoint
+@app.post("/users/", response_model=dict)
+def create_user(name: str, email:str, db:Session = Depends(get_db)):
+    result = user.add_new_user(name, email, db)
     return result
