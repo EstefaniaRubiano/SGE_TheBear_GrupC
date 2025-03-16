d101a48f70295c6e50b9619ad2a164b0024f22e3
alba0403 <2024_alba.melendres@iticbcn.cat>
2025-03-16 17:25:32 +0100

    exercici fastapi +

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # SGE_TheBear_GrupC
 
+<<<<<<< HEAD
 ![Captura 3](https://github.com/user-attachments/assets/d4935888-6e82-477a-b5fd-01f46a7e347b)
 En aquesta captura es pot veure tot el codi del projecte, fet a PyCharm. En 'main.py', s'està configurant FastAPI i definint l'endpoint 'GET /root' que retorna el resultat de la funció 'registre()' del mòdul 'read.py'. Aquesta funció crea un diccionari amb dades de tres usuaris. 
 A més, 'connect.py' estableix connexió amb la base de dades PostgreSQL mitjançant la llibreria 'psycopg2', la qual cosa permet gestionar les dades de manera més estructurada.
@@ -9,4 +10,6 @@ La següent imatge mostra la interfície Swagger UI de FastAPI, on s'està execu
 
 ![Captura 2](https://github.com/user-attachments/assets/da61430a-38ba-4474-b51d-2c6b1dfd0b4b)
 Finalment, a l'última imatge es pot veure la resposta d'aquesta petició, un JSON amb la llista d'usuaris emmagatzemats a l'API.
+=======
+>>>>>>> 8541a38 (exercici fastapi + db)
 
--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 from typing import List
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from services import read
+from sqlmodel import SQLModel, create_engine, Session
+from dotenv import load_dotenv
+from services import user
+import os
 
 app = FastAPI()
 
-@app.get( "/root", response_model=List[dict])
-async def read_root():
-    result = read.registre()
+# 1. Carregar variable d'entorn
+load_dotenv()
+
+# 2. Configurar la connexió a PostgreSQL
+DATABASE_URL = os.getenv("DATABASE_URL")  # Obtenir la URL de connexió des de .env
+engine = create_engine(DATABASE_URL)      # Crear l'engine de connexió
+
+# 3. Crear les taules a la base de dades
+SQLModel.metadata.create_all(engine)
+
+# 4. Crear l'aplicació FastAPI
+ # app = FastAPI()
+
+# 5. Dependència per obtenir la sessió de la base de dades
+def get_db():
+    db = Session(engine)
+    try:
+        yield db
+    finally:
+        db.close()
+
+# 6. Endpoint per llegir usuaris
+@app.get("/users/", response_model= list[dict])
+def read_user(db:Session = Depends(get_db)):
+    result = user.get_all_users(db)
+    return result
+
+# 7. Endpoint per afegir usuaris
+@app.post("/users/", response_model= dict)
+def read_user(name: str, email:str, db:Session = Depends(get_db)):
+    result = user.add_new_user(name, email, db)
     return result
